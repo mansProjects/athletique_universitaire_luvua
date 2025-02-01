56a5b2b6fe42f72a735880f79bb61a996bb72df0
mansanga <mansangajosue@gmail.com>
2025-02-01 23:03:59 +0100

    'aul'

---
+++ index.js
@@ -0,0 +1,7 @@
+let nameOfGuest = document.querySelector('.nameOfGuest')
+let guest = document.getElementById('guest')
+guest.addEventListener('click',()=>{
+location.href='allgusts.html'
+})
+
+    
